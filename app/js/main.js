015fe1b1c9a44d2dbda5d4bafa9ce4222805cfcf
darrenlim <darrenlzh@gmail.com>
2017-03-06 17:06:05 -0500

    Added mobile menu icon styles and JS scripts

---
+++ app/js/main.js
@@ -0,0 +1,23 @@
+ready(function(){
+  navMenuControl();
+});
+
+function ready(fn) {
+  if (document.readyState != 'loading') {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function navMenuControl() {
+  var navmenu = document.getElementById('navmenu');
+  navmenu.addEventListener('click', function() {
+    if (this.classList.contains('open')) {
+      this.classList.remove('open');
+    } else {
+      this.classList.add('open');
+    }
+    console.log('clicked');
+  });
+}
