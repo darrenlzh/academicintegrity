875314e64d8d16e1c869574a09c7cf5b8d3ab1c2
darrenlim <darrenlzh@gmail.com>
2017-03-08 17:11:04 -0500

    Added functionality to FAQ page: BringToView and BackToTop button

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 ready(function(){
   navMenuControl();
+  faqScrollControl();
+  window.addEventListener('scroll', function() {
+    showBackToTop();
+  });
 });
 
 function ready(fn) {
@@ -23,3 +27,48 @@ function navMenuControl() {
     }
   });
 }
+
+function faqScrollControl() {
+  var faq = document.getElementById('faq'),
+      list = faq.getElementsByTagName('li'),
+      questions = ['q1', 'q2', 'q3', 'q4', 'q5', 'q6'];
+  for (let i=0; i<list.length; i++) {
+    list[i].addEventListener('click', function(e) {
+      bringIntoView(document.getElementById(questions[i]), 500);
+    });
+  }
+}
+
+function showBackToTop() {
+  var scrollTop = document.body.scrollTop,
+      backToTop = document.getElementById('backtotop');
+  if (scrollTop > 800) {
+    backToTop.classList.add('show');
+  } else {
+    backToTop.classList.remove('show');
+  }
+}
+
+window.bringIntoView_started = 0;
+window.bringIntoView_ends = 0;
+window.bringIntoView_y = 0;
+window.bringIntoView_tick = function() {
+  var distanceLeft, dt, duration, t, travel;
+  t = Date.now();
+  if (t < window.bringIntoView_ends) {
+    dt = t - window.bringIntoView_started;
+    duration = window.bringIntoView_ends - window.bringIntoView_started;
+    distanceLeft = window.bringIntoView_y - document.body.scrollTop;
+      travel = distanceLeft * (dt / duration);
+      document.body.scrollTop += travel;
+      window.requestAnimationFrame(window.bringIntoView_tick);
+  } else {
+    document.body.scrollTop = window.bringIntoView_y;
+  }
+};
+window.bringIntoView = function(e, duration) {
+  window.bringIntoView_started = Date.now();
+  window.bringIntoView_ends = window.bringIntoView_started + duration;
+  window.bringIntoView_y = Math.min(document.body.scrollTop + e.getBoundingClientRect().top, document.body.scrollHeight - window.innerHeight);
+  window.requestAnimationFrame(window.bringIntoView_tick);
+};
